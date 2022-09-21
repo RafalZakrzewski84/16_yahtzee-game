346306c0e962df01a70b5c09fcc54b1d9ee1abd3
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-09-21 15:25:10 +0200

    16_108_Fixing the Dice Locking Bug

--- src/Die.js
+++ src/Die.js
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
-import "./Die.css";
+/** @format */
+
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
-  render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
-  }
+	render() {
+		return (
+			<button
+				className={'Die'}
+				style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
+				// changed to arrow function for binding this -fixing dice locking
+				onClick={() => this.props.handleClick(this.props.idx)}>
+				{this.props.val}
+			</button>
+		);
+	}
 }
 
 export default Die;
