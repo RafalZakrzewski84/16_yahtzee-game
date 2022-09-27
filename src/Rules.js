3cd23dbb92bd5de4ea0d7ccd70d5c57df222790a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-09-27 15:56:12 +0200

    16_110_adding missing rules

--- src/Rules.js
+++ src/Rules.js
@@ -73,14 +73,31 @@ class SumDistro extends Rule {
 
 /** Check if full house (3-of-kind and 2-of-kind) */
 
-class FullHouse {
+class FullHouse extends Rule {
 	// TODO
+	evalRoll = (dice) => {
+		const frqArr = this.freq(dice).sort();
+		return frqArr[0] === 2 && frqArr[1] === 3 ? this.score : 0;
+		//or
+		//const frqArr = this.freq(dice)
+		//return (frqArr.includes(2) && frqArr.includes(3) ? this.score : 0;
+	};
 }
 
 /** Check for small straights. */
 
-class SmallStraight {
+class SmallStraight extends Rule {
 	// TODO
+	evalRoll = (dice) => {
+		const d = new Set(dice);
+		if (d.has(2) && d.has(3) && d.has(4) && (!d.has(1) || !d.has(5))) {
+			return this.score;
+		}
+		if (d.has(3) && d.has(4) && d.has(5) && (!d.has(2) || !d.has(6))) {
+			return this.score;
+		}
+		return 0;
+	};
 }
 
 /** Check for large straights. */
@@ -97,10 +114,10 @@ class LargeStraight extends Rule {
 		3:4
 		4:5
 		size:5 */
-
 		// large straight must be 5 different dice & only one can be a 1 or a 6
 		// d.size is 5 and (!true = false) or (!false = true)
 		// true and true => score, e.g 40
+
 		return d.size === 5 && (!d.has(1) || !d.has(6)) ? this.score : 0;
 	};
 }
@@ -129,10 +146,10 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = 'TODO';
+const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = 'TODO';
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
